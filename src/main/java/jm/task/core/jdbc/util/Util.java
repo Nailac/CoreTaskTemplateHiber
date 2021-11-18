a4882e0495a2d523c1f53e118a3243594cc25e7a
Nailac <12gadzik98@gmail.com>
2021-11-18 19:56:38 +0300

    final

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,19 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public final static String user = "root";
+    public final static String password = "root";
+    public final static String url = "jdbc:mysql://localhost:3306/test?serverTimezone=UTC";
+
+    public static Connection createConnection(){
+        try {
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            throw new RuntimeException("Cannot create connection.", e);
+        }
+    }
 }
